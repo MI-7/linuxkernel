036a32b5dec8bc8b93b2f98a644bebd03f0ac400
leonchan1204 <leonchan1204@hotmail.com>
2018-01-03 18:51:53 +0800

    ..

--- notes/002-boot.s
+++ notes/002-boot.s
@@ -13,22 +13,22 @@ BOOTSEG = 0x07c0
 
 entry start
 start:
-      jmpi    go, BOOTSEG
-go:   mov     ax, cs
-      mov     ds, ax
-      mov     es, ax
-      mov     [msg+17], ah
-      mov     cx, #20
-      mov     dx, #0x1004
-      mov     bx, #0x000c
-      mov     bp, #msg
-      mov     ax, #0x1301
-      int     0x10
-loop1: jmp    loop1
-msg:  .ascii  "Lohahahahaha....."
-      .byte   13, 10
+      jmpi go,BOOTSEG
+go:   mov ax,cs
+      mov ds,ax
+      mov es,ax
+      mov [msg+17],ah
+      mov cx,#20
+      mov dx,#0x1004
+      mov bx,#0x000c
+      mov bp,#msg
+      mov ax,#0x1301
+      int 0x10
+loop1: jmp loop1
+msg:  .ascii "Lohahahahaha....."
+      .byte 13,10
 .org  510
-      .word   0xAA55
+      .word 0xAA55
 .text
 endtext:
 .data
