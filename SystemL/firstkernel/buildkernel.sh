05c1487a99c9093db505f1406fcb4a94db5f3286
leonchan1204 <leonchan1204@hotmail.com>
2018-01-18 10:29:40 +0800

    ..

--- SystemL/firstkernel/buildkernel.sh
+++ SystemL/firstkernel/buildkernel.sh
@@ -1,4 +1,4 @@
-rm boot.o kernel.o gdt_asm.o gdt.o utility.o test.o terminal.o io.o interrupt.o idt_asm.o idt.o isr_asm.o irq_asm.o
+rm boot.o kernel.o gdt_asm.o gdt.o utility.o test.o terminal.o io.o interrupt.o idt_asm.o idt.o isr_asm.o irq_asm.o isr.o
 i686-elf-as boot.s -o boot.o
 i686-elf-as gdt.s -o gdt_asm.o
 i686-elf-as idt.s -o idt_asm.o
@@ -11,7 +11,8 @@ i686-elf-gcc -c io.c -o io.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -c interrupt.c -o interrupt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -c test.c -o test.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -c idt.c -o idt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c isr.c -o isr.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 echo "linking..."
-i686-elf-gcc -T linker.ld -o systeml.bin -ffreestanding -O2 -nostdlib boot.o kernel.o gdt_asm.o gdt.o utility.o terminal.o test.o io.o interrupt.o idt_asm.o idt.o isr_asm.o -lgcc
+i686-elf-gcc -T linker.ld -o systeml.bin -ffreestanding -O2 -nostdlib boot.o kernel.o gdt_asm.o gdt.o utility.o terminal.o test.o io.o interrupt.o idt_asm.o idt.o isr_asm.o isr.o -lgcc
 ./testmultiboot.sh
 cp systeml.bin ./iso/boot/systeml.bin
